09ef91fa76c6fd180ccd54cd4866d29d3433c4e0
Joel <joel@create.net>
2013-08-10 02:56:51 +0100

    add back the 2 function versions

--- joint.js
+++ joint.js
@@ -1,7 +1,12 @@
+//----------------------------------------//
+// Just pick the most appropriate version //
+//----------------------------------------//
+
 /**
- * joint() joins an array of functions together to return a single function
+ * Joint for joining more than 2 functions.
+ * Joins an array of functions together to return a single function
  * @param  {array} a An array of functions
- * @return {function}   Returns a function which is an accumilation of functions in 'a'
+ * @return {function} Returns a function which is an accumilation of functions in 'a'
  */
 function joint( a ) {
 	var b;
@@ -17,6 +22,17 @@ function joint( a ) {
 	}
 
 	return function() {
-		b.apply(new a());
+		b.apply( new a() );
 	};
 }
+
+/**
+ * Joint for joining only 2 functions
+ * @param  {array} a Two functions in an array [func1, func2]
+ * @return {function} The first function with the second's added functionality
+ */
+function joint(a){
+	return function(){
+		a[1].apply( new a[0]() );
+	};
+};
